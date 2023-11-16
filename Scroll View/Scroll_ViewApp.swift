0306a1a006159909bb08cc59640dd3adfb0552d8
Yusuf Can Sağlam <135333933+yusufcansglm@users.noreply.github.com>
2023-11-16 04:00:37 -0800

    Initial Commit

---
+++ Scroll View/Scroll_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll_ViewApp.swift
+//  Scroll View
+//
+//  Created by yusuf can on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Scroll_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
